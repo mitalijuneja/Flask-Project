a7691a27f3fcf12ab969f0d7444a31e785c3a385
Mitali J <mitalijuneja1@mitalis-mbp-2.lan>
2020-05-02 12:36:06 -0700

    added homepage and css styling

--- final/app.py
+++ final/app.py
@@ -14,7 +14,11 @@ app = Flask(__name__)
 #static route
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template("index.html")
+
+@app.route("/1006")
+def engi():
+    return "1006 homepage"
 
 #start the server
 if __name__ == "__main__":
